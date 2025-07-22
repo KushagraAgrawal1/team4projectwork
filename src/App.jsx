4f31160205e1e5e979a41c32b515acbe5d44a9e8
Kushagra Agrawal <kushagraagrawald@gmail.com>
2025-07-22 18:51:36 +0530

    Made the heading mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,12 @@ function App() {
 
         {/* Main Content */}
   <div className="flex-1 p-6">
-<h1 className="sticky top-0 z-30 bg-slate-50 text-5xl font-extrabold text-center py-8 tracking-wider shadow-md border-b-4 border-purple-400">
+<h1 className="sticky top-0 z-30 bg-slate-50 text-3xl sm:text-4xl md:text-5xl font-extrabold text-center py-6 sm:py-8 tracking-wide shadow-md border-b-4 border-purple-400">
   <span className="bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent">
     Project ShowCase
   </span>
 </h1>
+
 
 
 
